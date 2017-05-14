10f30843b726bd0464ccd632e1c63384015cd312
simplicilee <solomon.maico.lee>
2017-05-13 18:32:00 -0700

    update again

--- js/style.js
+++ js/style.js
@@ -7,19 +7,19 @@ $(document).ready(function(){
 });
 
 
-		$(document).ready(function(){
-		  $('.nav li a').click(function() {
-		    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'')
-		    && location.hostname == this.hostname) {
-		      var $target = $(this.hash);
-		      $target = $target.length && $target
-		      || $('[name=' + this.hash.slice(1) +']');
-		      if ($target.length) {
-		        var targetOffset = $target.offset().top;
-		        $('html,body')
-		        .animate({scrollTop: targetOffset}, 1000);
-		       return false;
-		      }
-		    }
-		  });
+$(document).ready(function(){
+  $('a[href*=#]').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'')
+    && location.hostname == this.hostname) {
+      var $target = $(this.hash);
+      $target = $target.length && $target
+      || $('[name=' + this.hash.slice(1) +']');
+      if ($target.length) {
+        var targetOffset = $target.offset().top;
+        $('html,body')
+        .animate({scrollTop: targetOffset}, 1000);
+       return false;
+      }
+    }
+  });
 });
